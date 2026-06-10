363709e4d31e6a70a8117a9a4f1c0060399171fa
Tim Chen <timchen@ieiworld.com>
2026-06-10 11:55:42 +0800

    refactor: extract personal constants into config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  name:        "Tim Chen",
+  initials:    "TC",
+  title:       "Software Engineer",
+  org:         "IEI Integration",
+  email:       "timchen@ieiworld.com",
+  linkedinUrl: "https://www.linkedin.com/in/tim-chen-a51829161/",
+  githubUrl:   "https://github.com/InfernoPC",
+  pageUrl:     "https://infernopc.github.io/about-me",
+  vcfFile:     "tim.vcf",
+  vcfDownload: "Tim_Chen.vcf",
+};
