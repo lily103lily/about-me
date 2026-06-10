4464f5726033bae7599c32d497082e7f0c8787f0
Tim Chen <timchen@ieiworld.com>
2026-06-10 12:04:05 +0800

    update: correct organization name

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   name:        "Tim Chen",
   initials:    "TC",
   title:       "Project Manager",
-  org:         "IEI Integration",
+  org:         "IEI Integration Corp.",
   email:       "timchen@ieiworld.com",
   linkedinUrl: "https://www.linkedin.com/in/tim-chen-a51829161/",
   githubUrl:   "https://github.com/InfernoPC",
